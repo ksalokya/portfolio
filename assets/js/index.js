d62c1fa6bb3e6197a9611f4ecef40b5c798f1647
Salokya Kumar <kumar.salokya007@gmail.com>
2021-07-31 09:47:16 +0530

    fixed navbar and icon

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,20 @@
 var typed = new Typed(".typing1",{
     strings:["I'm Salokya Kumar."],
     typeSpeed:100,
-    backSpeed:60,
-    loop: false,
-    startDelay: 500,
-    smartBackspace: true,
+  	backSpeed:60,
+  	loop:true
 });
 
-//close navbar when nav item is clicked
-$(document).ready(function() {
-  $(document).click(function(event) {
-    var click = $(event.target);
-    var _open = $(".navbar-collapse").hasClass("show");
-    if (_open === true && !click.hasClass("navbar-toggler")) {
-      $(".navbar-toggler").click();
-    }
-  });
+
+
+//Close navbar when nav item is clicked
+
+$(".scrollTo").on('click', function(e) {
+   e.preventDefault();
+   var target = $(this).attr('href');
+    setTimeout(function(){
+      $('html, body').animate({
+        scrollTop: ($(target).offset().top)
+      }, 50);
+    },300)
 });
